3f4b56403f95615e9ec96b7270e9e92f9aecac86
unknown <graymanmiller@gmail.com>
2022-09-02 20:16:17 -0700

    final project

---
+++ src/scenes/Title.js
@@ -0,0 +1,40 @@
+class Title extends Phaser.Scene {
+    constructor() {
+        super("titleScene");
+    }
+
+    preload() {
+        // load assets
+        this.load.path = "./assets/";
+
+        // load JSON (dialog)
+        this.load.json('dialog', 'json/dialog.json');
+        this.load.json('macbeth', 'json/macbeth.json');
+
+        // load images
+        this.load.image('dialogbox', 'img/dialogbox.png');
+        this.load.image('menteith', 'img/menteith.png');
+        this.load.image('malcolm', 'img/malcolm.png');
+        this.load.image('soldiers', 'img/soldiers.png');
+        this.load.image('siward', 'img/siward.png');
+
+        // load bitmap font
+        this.load.bitmapFont('gem_font', 'font/gem.png', 'font/gem.xml');
+    }
+
+    create() {
+        // add title text
+        this.add.bitmapText(centerX, centerY - 32, 'gem_font', 'THE ODYSSEY', 32).setOrigin(0.5);
+        this.add.bitmapText(centerX, centerY, 'gem_font', 'Press SPACE to start', 16).setOrigin(0.5);
+
+        // create input
+        cursors = this.input.keyboard.createCursorKeys();
+    }
+
+    update() {
+        // wait for player input
+        if(Phaser.Input.Keyboard.JustDown(cursors.space)) {
+            this.scene.start("talkingScene");
+        }
+    }
+}
